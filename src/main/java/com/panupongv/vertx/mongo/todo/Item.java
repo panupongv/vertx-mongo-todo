4743600d186878e0448ee51a659cd0e39be0e9d2
pviriya <most54321@gmail.com>
2022-06-04 11:06:02 +0100

    #3 Utils.java: Json quote helper MongoVerticle.java: Create user + first consumer setup Item.java: Static final field names

--- src/main/java/com/panupongv/vertx/mongo/todo/Item.java
+++ src/main/java/com/panupongv/vertx/mongo/todo/Item.java
@@ -4,13 +4,20 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.bson.types.ObjectId;
+
 import io.vertx.core.json.JsonObject;
 
 public class Item {
-    public static final String COLLECTION_NAME = "items";
+    public static final String MONGO_ID_KEY = "_id";
+    public static final String NAME_KEY = "name";
+    public static final String DESCRIPTION_KEY = "description";
+    public static final String DUE_DATE_KEY = "due_date";
+    public static final String PRIORITY_KEY = "priority";
+
     static final DateFormat mongoDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
-    private String mongoId;
+    private ObjectId mongoId;
     private String name;
     private String description;
     private Date dueDate;
@@ -20,8 +27,8 @@ public class Item {
         this(null, name, description, dueDate, priority);
     }
 
-    public Item(String mongoId, String name, String description, Date dueDate, int priority) {
-        this.mongoId = mongoId;
+    public Item(ObjectId mongoId, String name, String description, Date dueDate, int priority) {
+        this.mongoId = ObjectId.get();
         this.name = name;
         this.description = description;
         this.dueDate = dueDate;
@@ -30,11 +37,11 @@ public class Item {
 
     public JsonObject getMongoDbJson() {
         JsonObject itemJson = new JsonObject()
-        //.put("id", mongoId == null? "":mongoId) "2022-06-05T00:00:00+00:00"
-        .put("name", name)
-        .put("description", description)
-        .put("due_date", mongoDateFormat.format(dueDate))
-        .put("priority", priority);
+        .put(MONGO_ID_KEY, mongoId.toString())
+        .put(NAME_KEY, name)
+        .put(DESCRIPTION_KEY, description)
+        .put(DUE_DATE_KEY, mongoDateFormat.format(dueDate))
+        .put(PRIORITY_KEY, priority);
 
         return itemJson;
     }
--- src/main/java/com/panupongv/vertx/mongo/todo/MongoVerticle.java
+++ src/main/java/com/panupongv/vertx/mongo/todo/MongoVerticle.java
@@ -1,42 +1,129 @@
 package com.panupongv.vertx.mongo.todo;
 
 import java.util.Date;
+import java.util.List;
+
+import org.bson.types.ObjectId;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.eventbus.Message;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.mongo.MongoClient;
 
 public class MongoVerticle extends AbstractVerticle {
 
+    public static final String CREATE_USER = "com.panupongv.vertx-todo.create_user";
+    public static final String LIST_ITEMS_BY_DUE_DATE = "com.panupongv.vertx-todo.list_items_by_due_date";
+    public static final String LIST_ITEMS_BY_PRIORITY = "com.panupongv.vertx-todo.list_items_by_priority";
+    public static final String GET_ITEM = "com.panupongv.vertx-todo.get_item";
+    public static final String EDIT_ITEM = "com.panupongv.vertx-todo.edit_item";
+    public static final String DELETE_ITEM = "com.panupongv.vertx-todo.delete_item";
+
+    public enum OperationOutcome {
+        SUCCEEDED,
+        FAILED
+    }
+
+    private static final String COLLECTION_NAME = "vertx_mongo_todos";
+    private static final String USERNAME_KEY = "username";
+    private static final String ITEMS_KEY = "items";
+
+    private enum SortOption {
+        BY_DATE,
+        BY_PRIORITY
+    }
+
+    private MongoClient mongoClient;
+
     @Override
-    public void start() {
+    public void start(Promise<Void> start) {
+        configureMongoDbClient()
+                .compose(this::configureEventBusConsumers)
+                .onComplete(start::handle);
+    }
+
+    private Future<Void> configureMongoDbClient() {
         String uri = "mongodb://localhost:27017";
         String db = "test-vertx";
 
         JsonObject mongoconfig = new JsonObject()
-            .put("connection_string", uri)
-            .put("db_name", db);
+                .put("connection_string", uri)
+                .put("db_name", db);
 
-        MongoClient mongoClient = MongoClient.createShared(vertx, mongoconfig);
+        mongoClient = MongoClient.createShared(vertx, mongoconfig);
 
-        Item item = new Item("Item name", "Some description", new Date(), 1178);
-        
-        mongoClient.save(Item.COLLECTION_NAME, item.getMongoDbJson())
-        .compose(id -> {
-            System.out.println("Inserted id: " + id);
-            return mongoClient.find("products", new JsonObject().put("itemId", "12345"));
-          })
-        //  .compose(res -> {
-        //    System.out.println("Name is " + res.get(0).getString("name"));
-        //    return mongoClient.removeDocument("products", new JsonObject().put("itemId", "12345"));
-        //  })
-          .onComplete(ar -> {
+        return Future.succeededFuture();
+    }
+
+    private Future<Void> configureEventBusConsumers(Void unused) {
+        System.out.println("Consumer Setup");
+
+        vertx.eventBus().consumer(CREATE_USER).handler(this::createUser);
+
+        return Future.succeededFuture();
+    }
+
+    private void createUser(Message<Object> msg) {
+        System.out.println("Create User Called");
+
+        String username = (String) msg.body();
+
+        userExists(username)
+                .onComplete(userExistsAsyncResult -> {
+                    boolean userExists = userExistsAsyncResult.result();
+                    if (userExists) {
+                        msg.fail(400, String.format("User '%s' already exists", username));
+                    } else {
+                        String jsonString = Utils
+                                .convertJsonQuotes(String.format("{'username': '%s', 'items': []}", username));
+                        JsonObject json = new JsonObject(jsonString);
+
+                        mongoClient.save(COLLECTION_NAME, json)
+                                .onComplete((asyncResult) -> {
+                                    if (asyncResult.succeeded()) {
+                                        msg.reply(String.format("User '%s' created", username));
+                                    } else {
+                                        msg.fail(500, asyncResult.cause().getMessage());
+                                    }
+                                });
+                    }
+                });
+    }
+
+    private void listItems(SortOption sortOption, Message<Object> msg) { // User name, sort options?
+        String username = (String) msg.body();
+
+    }
+
+    private void saveItem(String username, Item item) {
+        String queryJsonString = Utils.convertJsonQuotes(String.format("{'username': '%s'}", username));
+        JsonObject query = new JsonObject(queryJsonString);
+        System.out.println(query);
+
+        String updateJsonString = Utils.convertJsonQuotes(String.format(
+                "{'$push': {'items': %s}}",
+                item.getMongoDbJson().toString()));
+        System.out.println(updateJsonString);
+
+        JsonObject update = new JsonObject(updateJsonString);
+        System.out.println(update);
+
+        mongoClient.updateCollection(COLLECTION_NAME, query, update).onComplete(ar -> {
             if (ar.succeeded()) {
-              System.out.println("Done ");
+                System.out.println("Done ");
             } else {
-              ar.cause().printStackTrace();
+                System.out.println(ar.cause().getMessage());
             }
-          });
+        });
+    }
 
+    private Future<Boolean> userExists(String username) {
+        String findUserJsonString = Utils.convertJsonQuotes(String.format("{'username': '%s'}", username));
+        JsonObject findUserJson = new JsonObject(findUserJsonString);
+
+        return mongoClient.find(COLLECTION_NAME, findUserJson).map(users -> users.size() == 1);
     }
 }
