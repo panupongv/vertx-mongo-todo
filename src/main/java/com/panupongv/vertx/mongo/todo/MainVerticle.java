8a5ca53fcebac1fc9648ed03106f35082bbbf5eb
pviriya <most54321@gmail.com>
2022-06-21 19:30:32 +0100

    #3 MainVerticle.java: Made verticle deployment sequencial rather than parallel, will not host web is mongo is down

--- src/main/java/com/panupongv/vertx/mongo/todo/MainVerticle.java
+++ src/main/java/com/panupongv/vertx/mongo/todo/MainVerticle.java
@@ -1,23 +1,33 @@
 package com.panupongv.vertx.mongo.todo;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.CompositeFuture;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
-
 
 public class MainVerticle extends AbstractVerticle {
 
     @Override
     public void start(Promise<Void> start) {
-        deployVerticles().onComplete(start::handle);
+        deployMongoVerticle()
+                .compose(this::deployWebVerticle)
+                .onComplete(asyncResult -> {
+                    if (asyncResult.succeeded()) {
+                        System.out.println("Deployment Successful");
+                        start.handle(asyncResult);
+                    } else {
+                        String cause = asyncResult.cause().getMessage();
+                        System.out.println("Deployment Failed");
+                        System.out.println(cause);
+                        start.fail(cause);
+                    }
+                });
     }
 
-    Future<Void> deployVerticles() {
-        Future<Void> webVerticle = Future.future(promise -> vertx.deployVerticle(new WebVerticle()));
-        Future<Void> mongoVerticle = Future.future(promise -> vertx.deployVerticle(new MongoVerticle()));
-        
-        return CompositeFuture.all(webVerticle, mongoVerticle).mapEmpty();
+    Future<Void> deployMongoVerticle() {
+        return vertx.deployVerticle(new MongoVerticle()).mapEmpty();
     }
 
+    Future<Void> deployWebVerticle(Void unused) {
+        return vertx.deployVerticle(new WebVerticle()).mapEmpty();
+    }
 }
